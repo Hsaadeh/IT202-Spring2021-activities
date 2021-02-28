ad63f011e03f6e0131a69f6fddc0585603e1021e
Hsaadeh <hsaadeh622@gmail.com>
2021-02-28 16:33:25 -0600

    updates

--- Project 1/app.js
+++ Project 1/app.js
@@ -1,1 +1,15 @@
-let NavBarAnchors = document.querySelectorAll("ul#nav-pills a");
+let NavBarAnchors = document.querySelectorAll("ul#navbar a");
+
+NavBarAnchors.forEach( (anchor) => {
+    anchor.addEventListener("click", (event) => {
+        document.querySelectorAll(".screen"),forEach( (screen) => {
+            screen.style.display = "none";
+        });
+
+        let screen = event.target.getAttribute("data-screen");
+
+        let targetScreen = document.querySelectorAll("#" + screen);
+
+        targetScreen.style.display = "block";
+    })
+})
