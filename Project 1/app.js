9eecdda87d251bb7494b737f2a286828ec09b3de
Hsaadeh <hsaadeh622@gmail.com>
2021-02-28 20:19:28 -0600

    Cleaning
    Cleaned up some of the commented out code. Also moved the javascript to its own file called "app.js"

--- Project 1/app.js
+++ Project 1/app.js
@@ -1,15 +1,19 @@
+let home_screen = document.querySelector("#Home-Instructions");
+home_screen.style.display = "block";
+
 let NavBarAnchors = document.querySelectorAll("ul#navbar a");
 
 NavBarAnchors.forEach( (anchor) => {
     anchor.addEventListener("click", (event) => {
-        document.querySelectorAll(".screen"),forEach( (screen) => {
+        //console.log(":/")
+        document.querySelectorAll(".screen").forEach( (screen) => {
             screen.style.display = "none";
         });
-
+        
         let screen = event.target.getAttribute("data-screen");
-
-        let targetScreen = document.querySelectorAll("#" + screen);
-
+        let targetScreen = document.querySelector("#" + screen); //changed recently
         targetScreen.style.display = "block";
-    })
+    });
+
+    //console.log("oof");
 })
